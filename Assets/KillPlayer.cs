2593913e52b48c42985ffb5a19cc95eb9ae3456c
Tooooki <136748411+Tooooki@users.noreply.github.com>
2024-04-06 18:26:19 +0200

    killplayer

---
+++ Assets/KillPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Transform respownPoint;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("fortinit");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("dfghj");
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+            player.transform.position = respownPoint.position;
+        }
+    }
+
+}
