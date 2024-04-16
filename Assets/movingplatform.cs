8e018563f32150ca714f861ec3a69ce84294da74
LAB1-Student03 <lab1-student03@onlineszkolafilmowa.com.pl>
2024-04-16 11:48:33 +0200

    123

--- Assets/movingplatform.cs
+++ Assets/movingplatform.cs
@@ -1,27 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class movingplatform : MonoBehaviour
 {
     public Rigidbody rb;
-    public int movingSpeed = 1;
+    public float movingSpeed = 0.0001f;
+    public bool rightleft = false;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
     }
-
-    void Awake() 
-    {
-        rb.velocity = new Vector3(0, 0, movingSpeed);
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (rightleft)
+        {
+            transform.position = transform.position + new Vector3(movingSpeed, 0, 0);
+        }else
+        {
+            transform.position = transform.position + new Vector3(0, 0, movingSpeed);
+        }
 
     }
 
@@ -30,7 +32,6 @@ public class movingplatform : MonoBehaviour
         if (other.gameObject.CompareTag("Anchor")) 
         {
             movingSpeed = movingSpeed * -1;
-            rb.velocity = new Vector3(0, 0, movingSpeed);
         }
     }
 }
